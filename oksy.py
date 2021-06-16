35573052d02eabe841c2755b1d37b99df097f8c7
gek <detronics52@gmail.com>
2021-06-16 20:03:37 +0300

    накидал стартовое окно

--- oksy.py
+++ oksy.py
@@ -1,11 +1,11 @@
 import pytesseract
 import cv2
 
-x = 0
-y = 0
-h = 100
-w = 75
-img = cv2.imread("oks111.jpg")
+x = 235
+y = 120
+h = 105
+w = 90
+img = cv2.imread("oks11.jpg")
 crop_img = img[y:y+h, x:x+w]
 rgb = cv2.cvtColor(crop_img, cv2.COLOR_BGR2RGB)
 resize_img = cv2.resize(rgb, None, fx=9, fy=9)
