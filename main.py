1b121bebf645ec70cf8e37184456eeed2f74ea22
Konstantin <detronics52@gmail.com>
2021-06-24 16:32:23 +0300

    продолжил

--- main.py
+++ main.py
@@ -5,5 +5,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/personal_cabinet')
+def personal_cabinet():
+    return render_template('personal_cabinet.html')
+
+@app.route('/registration')
+def registration():
+    return render_template('registration.html')
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
