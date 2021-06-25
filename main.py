04ba74e0ab5df7b9a7e90b881301a2441d4087cb
Konstantin <detronics52@gmail.com>
2021-06-25 15:38:46 +0300

    сделал форму регистрации

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', title='Авторизация')
 
 @app.route('/personal_cabinet')
 def personal_cabinet():
-    return render_template('personal_cabinet.html')
+    return render_template('personal_cabinet.html', title='Личный кабинет')
 
-@app.route('/registration')
+@app.route('/registration', methods= ['POST', 'GET'])
 def registration():
-    return render_template('registration.html')
+    return render_template('registration.html', title='Регистрация')
 
 
 if __name__ == '__main__':
